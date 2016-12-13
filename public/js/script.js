d336cc663e7a2b678ee525c31184aeb6bbb12396
Tyler Bennett <tylerbennett215@gmail.com>
2016-12-13 14:02:46 -0500

    ungalo in my bungalo

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,6 @@
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1of8KfBDg4tfXjNFDM4ZJZh10NlTz4UQKx0ihGGI5vIA/edit?invite=CO61x-QI&ts=584850d8#gid=0';
 
+// Load an entire worksheet.
+$('#statistics').sheetrock({
+  url: mySpreadsheet
+});
